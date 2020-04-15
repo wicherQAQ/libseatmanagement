d0c1274f19ba334616e747aab357903c11ce82b7
wicher <weichao_wu@163.com>
2020-04-15 15:37:31 +0800

    处理并发的bug，添加定时回收座位

--- src/test/java/com/forecnu/libseatmanagement/LibseatmanagementApplicationTests.java
+++ src/test/java/com/forecnu/libseatmanagement/LibseatmanagementApplicationTests.java
@@ -1,34 +1,17 @@
 package com.forecnu.libseatmanagement;
 
-import com.forecnu.libseatmanagement.Thread.MyThread;
-import com.forecnu.libseatmanagement.entity.Seat;
-import com.forecnu.libseatmanagement.entity.User;
-import com.forecnu.libseatmanagement.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.concurrent.CountDownLatch;
 
 @SpringBootTest
 class LibseatmanagementApplicationTests {
 
-    @Autowired
-    UserService userService;
 
-    private static CountDownLatch countDownLatch = new CountDownLatch(3);
+
 
     @Test
     void contextLoads() {
-        for(int i=0;i<3;i++){
-            new Thread(new MyThread(userService,countDownLatch,i+1)).start();
-        }
 
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
 
