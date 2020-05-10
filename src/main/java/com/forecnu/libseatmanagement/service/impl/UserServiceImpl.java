8cadd53e2d424ee3f51cdbc4bfa4949730608002
wicher <weichao_wu@163.com>
2020-05-10 11:00:42 +0800

    fixed

--- src/main/java/com/forecnu/libseatmanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/forecnu/libseatmanagement/service/impl/UserServiceImpl.java
@@ -8,14 +8,12 @@ import com.forecnu.libseatmanagement.entity.User;
 import com.forecnu.libseatmanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.TimeoutUtils;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author wuwc
@@ -26,8 +24,10 @@ import java.time.Duration;
 @Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements UserService {
 
+
     private static final String HEAD = "SEAT_PREFIX_";
 
+    //注入模式单例的，普通属性和静态属性都会被共用。
     @Autowired
     private SeatMapper seatMapper;
 
@@ -37,35 +37,43 @@ public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements Use
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    private final Lock lock = new ReentrantLock();
+
     /**
      * 占座接口，测试实际可支持4000+的并发量
      * 可完美支持普通高效的占座需求
+     * 多线程副本执行代码段，
      * @param user 学生信息
      * @param seat 座位信息
      * @return
      */
     @Override
     public Boolean takeSeat(User user, Seat seat) {
-        //同步代码块，保证高并发环境下的原子性
-        synchronized (seatMapper){
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(HEAD + seat.getId(), user.getId().toString(), Duration.ofMillis(200));
-            if(result){
-                //如果设置成功，则可以抢
-                Seat st = seatMapper.selectById(seat.getId());
-                //判断是否已经被抢走了
-                if(!st.getAvailable()){
-                    return false;
-                }
-                st.setUser_id(user.getId());
-                st.setAvailable(false);
-                int back = seatMapper.updateById(st);
-                return back==1?true:false;
-            }else{
-                //设置失败说明已经有人抢到了
+        //先到redis中排队，设置1000毫秒有效时长,过滤同一时间的大量并发
+        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(HEAD + seat.getId(), user.getId().toString(), Duration.ofMillis(30000));
+        if(result){
+            //同一时间，只能有一个线程进来修改
+            //synchronized (seatMapper){
+            /*try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+            Seat st = seatMapper.selectById(seat.getId());
+            //判断是否已经被抢走了
+            if(!st.getAvailable()){
+                stringRedisTemplate.delete(HEAD + seat.getId());
                 return false;
             }
+            st.setUser_id(user.getId());
+            st.setAvailable(false);
+            seatMapper.updateById(st);
+            stringRedisTemplate.delete(HEAD + seat.getId());
+            return true;
+            //}
+        }else{
+            return false;
         }
-
     }
 
     @Override
